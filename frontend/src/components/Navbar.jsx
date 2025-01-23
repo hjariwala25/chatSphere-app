d36f0824a051899d4112775460fb5c0743d70ef3
hjariwala25 <hjariwala150@gmail.com>
2025-01-23 23:12:26 +0530

    add profile image upload functionality; integrate image compression and update user profile in useAuthStore

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
               <h1 className="text-lg font-bold">ChatSphere</h1>
             </Link>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-4">
           <Link
               to={"/settings"}
               className={`
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,4 +57,19 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      toast.error(error.response?.data?.message || "Error updating profile");
+      throw error;
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
