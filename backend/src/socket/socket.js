b3c02406206a20b22f7bfda877d5564569871f4b
hjariwala25 <hjariwala150@gmail.com>
2025-01-27 21:41:48 +0530

    feat(socket): implement online user tracking; emit online users on connection and disconnection

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -7,11 +7,23 @@ const io = new Server(server, {
   },
 });
 
+//to store online user
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if(userId) userSocketMap[userId] = socket.id;
+
+    //send online users to all clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  
+
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
